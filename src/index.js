ad71ebf735c40acb685b3805515c4432bc4d758e
logosghost <jimkim777@gmail.com>
2017-06-29 11:55:00 -0400

    start existing user method

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ module.exports = {
   containsWhiteSpace: containsWhiteSpace,
   containsTrailingSpace: containsTrailingSpace,
   isRequired: isRequired,
-  isNotANumber: isNotANumber
+  isNotANumber: isNotANumber,
+  isNotInRange: isNotInRange,
+  userAlreadyExists: userAlreadyExists
 };
 
 const errorMessages = {
@@ -13,22 +15,38 @@ const errorMessages = {
   white_space: 'must not contain spaces',
   trailing_space: 'must not begin with a space',
   required: 'is required',
-  not_a_number: 'is not a number'
+  not_a_number: 'is not a number',
+  not_in_range: 'is not in range',
+  user_exists_input_error: 'userAlreadyExists validator only accepts as the second argument: multi-dimensional array, array with objects, and array with strings'
 };
 
-const minimumLen = (input, num) => {
+/* helper methods for nameAlreadyExists */
+const validateArrays = users => {
+
+};
+
+const validateArrayOfObjects = users => {
+
+};
+
+const validateArrayOfStrings = users => {
+
+};
+
+/* validation methods */
+const minimumLen = (input, minLen) => {
   let len = input.length;
-  return (len > 0) && (len < num) ?
+  return (len > 0) && (len < minLen) ?
     errorMessages.too_short :
     null;
+
 };
 
 const containsWhiteSpace = input => (
   /\s/g.test(input)
 );
-
 const containsTrailingSpace = input => {
-  return input[ 0 ] === ' ' && input.length > 0 ?
+  return (input[ 0 ] === ' ') && (input.length > 0) ?
     messages.trailing_space :
     null
 };
@@ -45,6 +63,28 @@ const isNotANumber = num => {
     null
 };
 
-const isNotInRange = (num, range) => {
-  
+const isNotInRange = (num, rangeArray) => {
+  return (num < rangeArray[0]) || (num > rangeArray[1]) ?
+    errorMessages.not_in_range :
+    null
 };
+
+const userAlreadyExists = (input, users) => {
+  if (Array.isArray(users)) {
+    if (Array.isArray(users[0])) {
+      return validateArrays(users);
+    }
+    else if (typeof users[0] === 'object') {
+      return validateArrayOfObjects(users);
+    }
+    else if (typeof users[0] === 'string') {
+      return validateArrayOfStrings(users);
+    }
+  }
+  else {
+    throw errorMessages.user_exists_input_error;
+  }
+};
+
+
+
