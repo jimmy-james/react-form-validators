ffec8d6a99714bfc909c7a84561dac65a146aaf1
logosghost <jimkim777@gmail.com>
2017-06-28 22:27:07 -0400

    implement trailing space method

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ module.exports = {
 
 const errorMessages = {
   too_short: 'is too short',
-  white_space: 'must not contain white space'
+  white_space: 'must not contain spaces',
+  trailing_space: 'must not begin with a space',
 };
 
 const minimumLen = (input, num) => {
@@ -20,5 +21,10 @@ const containsWhiteSpace = input => (
   /\s/g.test(input)
 );
 
+const containsTrailingSpace = input => {
+  return input[ 0 ] === ' ' && input.length > 0 ?
+    messages.trailing_space :
+    null
+};
 
-
+const
